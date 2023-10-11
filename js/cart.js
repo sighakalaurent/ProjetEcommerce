3783a89ad8674fd15e55faf7209aba94e5602d7a
kazi kuete charnel <chanelkuete2@gmail.com>
2023-10-11 22:43:54 +0100

    cart css

--- js/cart.js
+++ js/cart.js
@@ -3,3 +3,17 @@ fetch('produits.json')
 .then(data => {
 console.log(data);
 });
+let cart_picture = document.querySelector("cart_picture")
+let name_products = document.querySelector("name_products")
+let price_products = document.querySelector("price_products")
+let category_products = document.querySelector("category_products")
+
+function cart(products){
+    localStorage.setItem("products", JSON.stringify(products))
+}
+function getCart(){
+   let products = localStorage.getItem("products")
+}
+function addCart(product){
+
+}
