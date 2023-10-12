1a41183426f4f2cb3a6f2257c38504cda1bc0aaf
sighakalaurent <nguifosighaka>
2023-10-12 19:40:16 +0200

    gestionrecherche

--- js/tousproduits.js
+++ js/tousproduits.js
@@ -1,28 +1,67 @@
-// let products
-// const listproduits = document.querySelector(".tousProduits")
-// fetch('produits.json').then(response => response.json().then(data => {
-//     products = data
-//     for (let i = 0; i < products.length; i++) {
-//         listproduits.innerHTML += `
-//             <div class="produits">
-//             <div class="image"> <img src="../asset/img/${products[i].image}" alt="">
-//             </div>
-//             <div class="p1"><p>${products[i].nom}</p></div>
-//             <div class="p2"> <p>${products[i].categorie}</p></div>
-//             <div class="p3">
-//                 <p>${products[i].prix} XFA</p>
-//             </div>
-//             <div class="btn"><button>Ajoute au Panier</button></div>
-//             </div>`
+const searchInput = document.getElementById('search');
+const listproduitsR = document.querySelector(".tousProduits")
+const toutecategorie =document.getElementById('boutontous')
 
-//     }
-// }
-// ))
-// `
-// <div class="Produits">
-// <a href="details.html"><img id="img" src= ../asset/img/${products[i].image} alt="charset"></a>
-// <p id="titre">${products[i].nom}</p>
-// <p id="description">${products[i].categorie}</p>
-// <p id="prix"><span>${products[i].prix} XAF</span></p>
-//  <button id="btn_produits" onclick="ajouteraupanier(1)" >Ajouter au panier</button>
-// </div>`
+let products
+fetch('produits.json').then(response => response.json().then(data => {
+    products = data
+console.log(data)
+    for (let i = 0; i < products.length; i++) {
+        listproduitsR.innerHTML += `
+            <div class="produits">
+            <div class="image"> <img src="../asset/img/${products[i].image}" alt="">
+            </div>
+            <div class="p1"><p>${products[i].nom}</p></div>
+            <div class="p2"> <p>${products[i].categorie}</p></div>
+            <div class="p3">
+                <p>${products[i].prix} XFA</p>
+            </div>
+            <div class="btn"><button>Ajoute au Panier</button></div>
+            </div>`
+
+    }
+
+        searchInput.addEventListener('keyup', () => {
+        const input = searchInput.value;
+        console.log(input)
+        const result = products.filter(produit => produit.nom.toLowerCase().includes(input.toLowerCase()))
+        valuesearch = result
+        console.log(valuesearch)
+        listproduitsR.innerHTML = ""
+
+        for (let i = 0; i < valuesearch.length; i++) {
+            listproduitsR.innerHTML += `
+                        <div class="produits">
+                        <div class="image"> <img src="../asset/img/${valuesearch[i].image}" alt="">
+                        </div>
+                        <div class="p1"><p>${valuesearch[i].nom}</p></div>
+                        <div class="p2"> <p>${valuesearch[i].categorie}</p></div>
+                        <div class="p3">
+                            <p>${valuesearch[i].prix} XFA</p>
+                        </div>
+                        <div class="btn"><button>Ajoute au Panier</button></div>
+                        </div>`
+        }
+    })
+    toutecategorie.addEventListener('click',()=>{
+
+        for (let i = 0; i < products.length; i++) {
+            listproduitsR.innerHTML += `
+                <div class="produits">
+                <div class="image"> <img src="../asset/img/${products[i].image}" alt="">
+                </div>
+                <div class="p1"><p>${products[i].nom}</p></div>
+                <div class="p2"> <p>${products[i].categorie}</p></div>
+                <div class="p3">
+                    <p>${products[i].prix} XFA</p>
+                </div>
+                <div class="btn"><button>Ajoute au Panier</button></div>
+                </div>`
+               
+        }                   })
+
+})
+
+
+)
+
