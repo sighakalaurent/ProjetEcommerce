9290561cf35f51ceab3a6d8db681fd2ec07d46eb
sighakalaurent <nguifosighaka>
2023-10-12 20:32:00 +0200

    categorie dynamique

--- js/tousproduits.js
+++ js/tousproduits.js
@@ -1,13 +1,18 @@
-const searchInput = document.getElementById('search');
-const listproduitsR = document.querySelector(".tousProduits")
-const toutecategorie =document.getElementById('boutontous')
+// recuperation des donnees
 
-let products
-fetch('produits.json').then(response => response.json().then(data => {
-    products = data
-console.log(data)
+const searchInput = document.getElementById("search");
+const listproduitsR = document.querySelector(".tousProduits");
+const toutecategorie = document.getElementById("boutontous");
+const ameublements = document.getElementById("ameublement");
+const Decorations= document.getElementById('decoration')
+const Vaisselles = document.getElementById('vaisselles')
+let products;
+fetch("produits.json").then((response) =>
+  response.json().then((data) => {
+    products = data;
+
     for (let i = 0; i < products.length; i++) {
-        listproduitsR.innerHTML += `
+      listproduitsR.innerHTML += `
             <div class="produits">
             <div class="image"> <img src="../asset/img/${products[i].image}" alt="">
             </div>
@@ -17,36 +22,38 @@ console.log(data)
                 <p>${products[i].prix} XFA</p>
             </div>
             <div class="btn"><button>Ajoute au Panier</button></div>
-            </div>`
-
+            </div>`;
     }
 
-        searchInput.addEventListener('keyup', () => {
+    searchInput.addEventListener("keyup", () => {
         const input = searchInput.value;
-        console.log(input)
-        const result = products.filter(produit => produit.nom.toLowerCase().includes(input.toLowerCase()))
-        valuesearch = result
-        console.log(valuesearch)
-        listproduitsR.innerHTML = ""
-
+        console.log(input);
+        const result = products.filter((produit) =>
+          produit.nom.toLowerCase().includes(input.toLowerCase())
+        );
+        valuesearch = result;
+        console.log(valuesearch);
+        listproduitsR.innerHTML = "";
+  
         for (let i = 0; i < valuesearch.length; i++) {
-            listproduitsR.innerHTML += `
-                        <div class="produits">
-                        <div class="image"> <img src="../asset/img/${valuesearch[i].image}" alt="">
-                        </div>
-                        <div class="p1"><p>${valuesearch[i].nom}</p></div>
-                        <div class="p2"> <p>${valuesearch[i].categorie}</p></div>
-                        <div class="p3">
-                            <p>${valuesearch[i].prix} XFA</p>
-                        </div>
-                        <div class="btn"><button>Ajoute au Panier</button></div>
-                        </div>`
+          listproduitsR.innerHTML += `
+                          <div class="produits">
+                          <div class="image"> <img src="../asset/img/${valuesearch[i].image}" alt="">
+                          </div>
+                          <div class="p1"><p>${valuesearch[i].nom}</p></div>
+                          <div class="p2"> <p>${valuesearch[i].categorie}</p></div>
+                          <div class="p3">
+                              <p>${valuesearch[i].prix} XFA</p>
+                          </div>
+                          <div class="btn"><button>Ajoute au Panier</button></div>
+                          </div>`;
         }
-    })
-    toutecategorie.addEventListener('click',()=>{
+      });
 
-        for (let i = 0; i < products.length; i++) {
-            listproduitsR.innerHTML += `
+    toutecategorie.addEventListener("click", () => {
+      listproduitsR.innerHTML = "";
+      for (let i = 0; i < products.length; i++) {
+        listproduitsR.innerHTML += `
                 <div class="produits">
                 <div class="image"> <img src="../asset/img/${products[i].image}" alt="">
                 </div>
@@ -56,12 +63,114 @@ console.log(data)
                     <p>${products[i].prix} XFA</p>
                 </div>
                 <div class="btn"><button>Ajoute au Panier</button></div>
-                </div>`
-               
-        }                   })
+                </div>`;
+      }
+    });
 
-})
+    ameublements.addEventListener("click", () => {
+      const categorie = "Ameublement";
+      const result = products.filter((produit) => produit.categorie == categorie);
 
+      listproduitsR.innerHTML = "";
 
-)
+      for (let i = 0; i < result.length; i++) {
+        listproduitsR.innerHTML += `
+                            <div class="produits">
+                            <div class="image"> <img src="../asset/img/${result[i].image}" alt="">
+                            </div>
+                            <div class="p1"><p>${result[i].nom}</p></div>
+                            <div class="p2"> <p>${result[i].categorie}</p></div>
+                            <div class="p3">
+                                <p>${result[i].prix} XFA</p>
+                            </div>
+                            <div class="btn"><button>Ajoute au Panier</button></div>
+                            </div>`;
+      }
+    });
+    Decorations.addEventListener("click", () => {
+        const categorie = "Decoration";
+        const result = products.filter((produit) => produit.categorie == categorie);
+  
+        listproduitsR.innerHTML = "";
+  
+        for (let i = 0; i < result.length; i++) {
+          listproduitsR.innerHTML += `
+                              <div class="produits">
+                              <div class="image"> <img src="../asset/img/${result[i].image}" alt="">
+                              </div>
+                              <div class="p1"><p>${result[i].nom}</p></div>
+                              <div class="p2"> <p>${result[i].categorie}</p></div>
+                              <div class="p3">
+                                  <p>${result[i].prix} XFA</p>
+                              </div>
+                              <div class="btn"><button>Ajoute au Panier</button></div>
+                              </div>`;
+        }
+      });
+     Vaisselles.addEventListener("click", () => {
+        const categorie = "vaisselles";
+        const result = products.filter((produit) => produit.categorie == categorie);
+  
+        listproduitsR.innerHTML = "";
+  
+        for (let i = 0; i < result.length; i++) {
+          listproduitsR.innerHTML += `
+                              <div class="produits">
+                              <div class="image"> <img src="../asset/img/${result[i].image}" alt="">
+                              </div>
+                              <div class="p1"><p>${result[i].nom}</p></div>
+                              <div class="p2"> <p>${result[i].categorie}</p></div>
+                              <div class="p3">
+                                  <p>${result[i].prix} XFA</p>
+                              </div>
+                              <div class="btn"><button>Ajoute au Panier</button></div>
+                              </div>`;
+        }
+      });
+  })
+);
 
+// const showmore = document.getElementById('btn')
+// const datajson = fetch('produits.json').then(response => response.json().then(data => {
+//     products = data
+
+//     for (let i = 0; i < products.length; i++) {
+//         listproduitsR.innerHTML += `
+//             <div class="produits">
+//             <div class="image"> <img src="../asset/img/${products[i].image}" alt="">
+//             </div>
+//             <div class="p1"><p>${products[i].nom}</p></div>
+//             <div class="p2"> <p>${products[i].categorie}</p></div>
+//             <div class="p3">
+//                 <p>${products[i].prix} XFA</p>
+//             </div>
+//             <div class="btn"><button>Ajoute au Panier</button></div>
+//             </div>`
+
+//     //     /*   if (valueproduit.includes(searchInput)) {
+//     //           products.style.display = 'block'
+//     //       } else {
+//     //           products.style.display = 'none'
+//     //       }        */
+//     }
+
+//  }
+// ))
+// let inputData = ""
+// async function searchProduct() {
+//     inputData = searchInput.value;
+//     const products = `
+//     <div class="produits">
+//     <div class="image"> <img src="../asset/img/${products.image}" alt="">
+//     </div>
+//     <div class="p1"><p>${products.nom.inputData}</p></div>
+//     <div class="p2"> <p>${products.categorie}</p></div>
+//     <div class="p3">
+//         <p>${products.prix} XFA</p>
+//     </div>
+//     <div class="btn"><button>Ajoute au Panier</button></div>
+//     </div>`
+
+//     if ()
+
+// }
