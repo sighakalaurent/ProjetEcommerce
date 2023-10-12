83808cf2adb2aad68cdb18f49dedbb7bac0dbd08
kazi kuete charnel <chanelkuete2@gmail.com>
2023-10-12 11:07:46 +0100

    changes in the code

--- js/cart.js
+++ js/cart.js
@@ -8,12 +8,4 @@ let name_products = document.querySelector("name_products")
 let price_products = document.querySelector("price_products")
 let category_products = document.querySelector("category_products")
 
-function cart(products){
-    localStorage.setItem("products", JSON.stringify(products))
-}
-function getCart(){
-   let products = localStorage.getItem("products")
-}
-function addCart(product){
-
-}
+let cart = document.querySelector("cart")
