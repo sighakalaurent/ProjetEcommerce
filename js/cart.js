9f04ea5becfaf31296fe8118f479d8a75274c6dd
kazi kuete charnel <chanelkuete2@gmail.com>
2023-10-13 14:50:53 +0100

    add to show product

--- js/cart.js
+++ js/cart.js
@@ -7,13 +7,10 @@ function showCart(products) {
             cartDiv.innerHTML += 
             `<div class="cart_content">
         <div class="cart_picture">
-            <img
-              src="../asset/img/${products[i].image}.png"
-              alt=""
-            />
+        <img id="img" src= ../asset/img/${products[i].image} alt="charset">
           </div>
             <div class="cart_layout">
-              <div class="name_products">Chaise de bureau <span><img src="../asset/delete-outline.svg" alt=""></span></div>
+              <div class="name_products"> ${products[i].nom} <span><img src="../asset/delete-outline.svg" alt=""></span></div>
               <div class="price_products">${products[i].prix}XAF</div>
               <div class="category_products">${products[i].categorie}</div>
               <div class="quantite_product">
@@ -22,19 +19,17 @@ function showCart(products) {
                 <input type="button" value="1" />
                 <input type="button" value="+" />
               </div>
-              <div class="sous_total">Sous-total:<span>25.000XAF</span></div>
+              <div class="sous_total">Prix unitaire:<span>25.000XAF</span></div>
               <div class="total">total:<span>25.000XAF</span></div>
-              <div class="btn_commandez">
-                <input type="button" value="Commandez" />
-              </div> 
+              
             </div>
             
         </div>
       ` }
     }
 ))
+console.log('bonjour')
 }
 let products= sessionStorage.getItem("products")
 let cart = JSON.parse(sessionStorage.getItem(products))
 showCart(products);
-console("hhkkfsdkfds")
