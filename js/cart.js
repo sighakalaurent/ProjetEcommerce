03f6feacbe409fd6ead802748cef20d59e87ea00
sighakalaurent <nguifosighaka>
2023-10-17 12:44:53 +0200

    d

--- js/cart.js
+++ js/cart.js
@@ -8,9 +8,6 @@ let cart_picture = document.querySelector("cart_picture")
 let name_products = document.querySelector("name_products")
 let price_products = document.querySelector("price_products")
 let category_products = document.querySelector("category_products")
-
-let cart = document.querySelector("cart")
-
 let cartDiv = document.querySelector(".container_cart");
 
 function getProductsFromLocalStorage(products) {
